255db994ea64728dcb8a525075d304d4ec53d185
Albin L. Jones <albin.jones@gmail.com>
2017-09-14 00:35:38 -0700

    Reorganize device.

--- ztex.go
+++ ztex.go
@@ -2,84 +2,174 @@
 package ztex
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/google/gousb"
 )
 
-var (
+const (
 	// VendorID is the ZTEX USB vendor ID (VID).
 	VendorID = gousb.ID(0x221A)
-
 	// ProductID is the standard ZTEX USB product ID (PID)
 	ProductID = gousb.ID(0x0100)
 
-	// ControlTimeout is the timeout for control transfers.
+	// ControlTimeout is the default timeout for control transfers.
 	ControlTimeout = 1000 * time.Millisecond
 )
 
+// FirmwareVersion indicates which version of the ZTEX firmware is
+// present.  Currently only FX1 and FX2 are supported.
+type FirmwareVersion uint8
+
+// String returns a human-readable description of a firmware version.
+func (f FirmwareVersion) String() string {
+	switch f {
+	case 2:
+		return "FX2"
+	case 3:
+		return "FX3"
+	case 255:
+		fallthrough
+	default:
+		return "Unknown"
+	}
+}
+
+// Number returns the raw representation of a firmware version.
+func (f FirmwareVersion) Number() uint8 { return uint8(f) }
+
+// BoardSeries indicates an entire generation of boards.  Currently only
+// Series 1 and Series 2 boards are supported.
+type BoardSeries uint8
+
+// String returns a human-readable description of a board series.
+func (b BoardSeries) String() string {
+	switch b {
+	case 1:
+		return "1"
+	case 2:
+		return "2"
+	default:
+		return "Unknown"
+	}
+}
+
+// Number returns the raw representation of a board series.
+func (b BoardSeries) Number() uint8 { return uint8(b) }
+
+// BoardNumber indicates a board in a series.
+type BoardNumber uint8
+
+// String returns a human-readable description of a board number.
+func (b BoardNumber) String() string {
+	if b == 255 {
+		return "Unknown"
+	}
+	return fmt.Sprintf("%d", uint8(b))
+}
+
+// Number returns the raw representation of a board number.
+func (b BoardNumber) Number() uint8 { return uint8(b) }
+
+// BoardVariant indicates a variation on a board series and number.
+type BoardVariant []byte
+
+// String returns a human-readable description of a board variant.
+func (b BoardVariant) String() string { return string(b) }
+
+// Number returns the raw representation of a board variant.
+func (b BoardVariant) Bytes() []byte { return []byte(b) }
+
+// BoardVersion comprises the series, number, and variant of a ZTEX USB
+// FPGA module.  For example, a ZTEX USB FPGA module 2.14b device would be
+// represented by
+//
+//   BoardVersion{
+//     Series: BoardSeries(2),
+//     Number: BoardNumber(14),
+//     Variant: BoardVariant("b"),
+//   }
+//
+// as a BoardVersion structure.
+type BoardVersion struct {
+	Series  BoardSeries
+	Number  BoardNumber
+	Variant BoardVariant
+}
+
+// String returns a human-readable representation of a board version.
+func (b BoardVersion) String() string {
+	return b.Series.String() + "." + b.Number.String() + b.Variant.String()
+}
+
 // Device represents a ZTEX USB FPGA module.
 type Device struct {
-	ProductName  [128]byte
-	SerialNumber [64]byte
-	FXVersion    byte    // 2 = FX2, 3 = FX3.
-	BoardSeries  byte    // 1 = Series 1, 2 = Series 2.  (255 = Unknown)
-	BoardNumber  byte    // E.g., 2.14b -> 2.  (255 = Unknown)
-	BoardVariant [2]byte // E.g., 2.14b -> [2]byte{'b', 0}.
+	*gousb.Device
+
+	Firmware FirmwareVersion
+	Board    BoardVersion
 
 	// Endpoint for fast FPGA configuration.  (0 = Unsupported)
 	FastConfigurationEndpoint byte
 	// Interface for fast FPGA configuration.  (0 = Unsupported)
 	FastConfigurationInterface byte
-	/// Default interface major version number.  (0 = Not Available)
+	// Default interface major version number.  (0 = Not Available)
 	DefaultInterfaceMajorVersion byte
-	/// Default interface minor version number.  (0 = Not Available)
+	// Default interface minor version number.  (0 = Not Available)
 	DefaultInterfaceMinorVersion byte
-	/// Output endpoint of default interface.  (255 = Not Available)
+	// Output endpoint of default interface.  (255 = Not Available)
 	DefaultInterfaceOutputEndpoint byte
-	/// Input endpoint of default interface.  (255 = Not Available)
+	// Input endpoint of default interface.  (255 = Not Available)
 	DefaultInterfaceInputEndpoint byte
-
-	// Device is the USB device handle for the module.
-	Device *gousb.Device
 }
 
+// OpenDevice opens the
 func OpenDevice(ctx *gousb.Context) (*Device, error) {
 	d := &Device{}
-
 	if dev, err := ctx.OpenDeviceWithVIDPID(VendorID, ProductID); err != nil {
 		return nil, err
+	} else if dev == nil {
+		return nil, errors.New("no device")
 	} else {
-		dev.ControlTimeout = ControlTimeout
 		d.Device = dev
+		d.Device.ControlTimeout = ControlTimeout
 	}
 
 	buf := make([]byte, 128)
 
+	// VR 0x3b: MAC EEPROM support: Read from MAC EEPROM
+	if nbt, err := d.Control(0xc0, 0x3b, 0, 0, buf); err != nil {
+		return nil, err
+	} else if nbt == 128 {
+		d.Firmware = FirmwareVersion(buf[3])
+		d.Board = BoardVersion{
+			Series:  BoardSeries(buf[4]),
+			Number:  BoardNumber(buf[5]),
+			Variant: BoardVariant(buf[6:8]),
+		}
+	} else {
+		d.Firmware = FirmwareVersion(255)
+		d.Board = BoardVersion{
+			Series:  BoardSeries(255),
+			Number:  BoardNumber(255),
+			Variant: BoardVariant([]byte{255, 255}),
+		}
+	}
+
 	// VR 0x33: High speed FPGA configuration support: Read Endpoint settings
-	if nbt, err := d.Device.Control(0xc0, 0x33, 0, 0, buf); err != nil {
+	if nbt, err := d.Control(0xc0, 0x33, 0, 0, buf); err != nil {
 		return nil, err
 	} else if nbt == 2 {
 		d.FastConfigurationEndpoint = buf[0]
 		d.FastConfigurationInterface = buf[1]
-	}
-
-	// VR 0x3b: MAC EEPROM support: Read from MAC EEPROM
-	if nbt, err := d.Device.Control(0xc0, 0x3b, 0, 0, buf); err != nil {
-		return nil, err
-	} else if nbt == 128 {
-		d.FXVersion = buf[3]
-		d.BoardSeries = buf[4]
-		d.BoardNumber = buf[5]
-		d.BoardVariant[0] = buf[6]
-		d.BoardVariant[1] = buf[7]
 	} else {
-		d.BoardSeries = 255
-		d.BoardNumber = 255
+		return nil, errors.New("internal error")
 	}
 
 	// VR 0x64: Default firmware interface: Return Default Interface information
-	if nbt, err := d.Device.Control(0xc0, 0x64, 0, 0, buf); err != nil {
+	if nbt, err := d.Control(0xc0, 0x64, 0, 0, buf); err != nil {
 		return nil, err
 	} else if nbt == 4 {
 		d.DefaultInterfaceMajorVersion = buf[0]
@@ -95,30 +185,5 @@ func OpenDevice(ctx *gousb.Context) (*Device, error) {
 		d.DefaultInterfaceInputEndpoint = 255
 	}
 
-	if prd, err := d.Device.Product(); err != nil {
-		return nil, err
-	} else {
-		for i, b := range []byte(prd) {
-			if i >= 128 {
-				break
-			}
-			d.ProductName[i] = b
-		}
-	}
-
-	if srl, prd, err := d.Device.SerialNumber(); err != nil {
-		return nil, err
-	} else {
-		for i, b := range []byte(srl) {
-			if i >= 128 {
-				break
-			}
-			d.ProductName[i] = b
-		}
-	}
-
 	return d, nil
 }
-
-// Close releases all external resources associated with the device.
-func (d *Device) Close() error { return d.Device.Close() }
