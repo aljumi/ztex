4806216adc555f5479ecd98b21f7b3ee89c574e6
Albin L. Jones <albin.jones@gmail.com>
2017-09-18 02:09:49 -0700

    Remove bytes from device config description.

--- ztex.go
+++ ztex.go
@@ -376,7 +376,6 @@ func (d *Device) String() string {
 	lines = append(lines, fmt.Sprintf("FPGA: %v", d.FPGAConfig))
 	lines = append(lines, fmt.Sprintf("RAM: %v", d.RAMConfig))
 	lines = append(lines, fmt.Sprintf("Bitstream: %v", d.BitstreamConfig))
-	lines = append(lines, fmt.Sprintf("Bytes: %v", d.Bytes))
 
 	return strings.Join(lines, "\n")
 }
