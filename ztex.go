46583dc588d8616f2d28b66df66f6e84ed90048a
Albin L. Jones <albin.jones@gmail.com>
2017-09-19 08:52:06 -0700

    Change count to n in binaryPrefix.

--- ztex.go
+++ ztex.go
@@ -20,16 +20,16 @@ const (
 	Unknown = "Unknown"
 )
 
-func binaryPrefix(count uint64, unit string) string {
+func binaryPrefix(n uint64, unit string) string {
 	switch {
-	case count != 0 && count&(1<<30-1) == 0:
-		return fmt.Sprintf("%vGi%v (%v%v)", count>>30, unit, count, unit)
-	case count != 0 && count&(1<<20-1) == 0:
-		return fmt.Sprintf("%vMi%v (%v%v)", count>>20, unit, count, unit)
-	case count != 0 && count&(1<<10-1) == 0:
-		return fmt.Sprintf("%vki%v (%v%v)", count>>10, unit, count, unit)
+	case n != 0 && n&(1<<30-1) == 0:
+		return fmt.Sprintf("%vGi%v (%v%v)", n>>30, unit, n, unit)
+	case n != 0 && n&(1<<20-1) == 0:
+		return fmt.Sprintf("%vMi%v (%v%v)", n>>20, unit, n, unit)
+	case n != 0 && n&(1<<10-1) == 0:
+		return fmt.Sprintf("%vki%v (%v%v)", n>>10, unit, n, unit)
 	default:
-		return fmt.Sprintf("%v%v", count, unit)
+		return fmt.Sprintf("%v%v", n, n)
 	}
 }
 
