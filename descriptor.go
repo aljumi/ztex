2ac150a00d0351eea456a6d2b1e7ff99e0eb8c28
Albin L. Jones <albin.jones@gmail.com>
2017-09-23 19:08:07 -0700

    Reorganize and rename.

---
+++ descriptor.go
@@ -0,0 +1,172 @@
+package ztex
+
+import (
+	"fmt"
+	"strings"
+)
+
+// DescriptorSize represents the number of bytes in a ZTEX descriptor.
+type DescriptorSize uint8
+
+// DescriptorVersion represents the version of a ZTEX descriptor.
+type DescriptorVersion uint8
+
+// DescriptorMagic indicates the presence of a ZTEX descriptor.
+type DescriptorMagic [4]uint8
+
+// String returns a human-readable description of the ZTEX magic bytes.
+func (d DescriptorMagic) String() string { return string(d.Bytes()) }
+
+// Bytes returns a raw representation of the ZTEX magic bytes.
+func (d DescriptorMagic) Bytes() []byte { return []byte{d[0], d[1], d[2], d[3]} }
+
+// DescriptorProduct represents a ZTEX product ID.
+type DescriptorProduct [4]uint8
+
+// String returns a human-readable description of the ZTEX product ID.
+func (d DescriptorProduct) String() string {
+	p := Unknown
+	switch {
+	case d[0] == 0 && d[1] == 0 && d[2] == 0 && d[3] == 0:
+		p = "Default"
+	case d[0] == 1:
+		p = "Experimental"
+	case d[0] == 10 && d[1] == 0 && d[2] == 1 && d[3] == 1:
+		p = "ZTEX BTCMiner"
+	case d[0] == 10 && d[1] == 11:
+		p = "ZTEX USB-FPGA Module 1.2"
+	case d[0] == 10 && d[1] == 12 && d[2] == 2 && (1 <= d[3] && d[3] <= 4):
+		p = "NIT"
+	case d[0] == 10 && d[1] == 12:
+		p = "ZTEX USB-FPGA Module 1.11"
+	case d[0] == 10 && d[1] == 13:
+		p = "ZTEX USB-FPGA Module 1.15"
+	case d[0] == 10 && d[1] == 14:
+		p = "ZTEX USB-FPGA Module 1.15x"
+	case d[0] == 10 && d[1] == 15:
+		p = "ZTEX USB-FPGA Module 1.15y"
+	case d[0] == 10 && d[1] == 16:
+		p = "ZTEX USB-FPGA Module 2.16"
+	case d[0] == 10 && d[1] == 17:
+		p = "ZTEX USB-FPGA Module 2.13"
+	case d[0] == 10 && d[1] == 18:
+		p = "ZTEX USB-FPGA Module 2.01"
+	case d[0] == 10 && d[1] == 19:
+		p = "ZTEX USB-FPGA Module 2.04"
+	case d[0] == 10 && d[1] == 20:
+		p = "ZTEX USB Module 1.0"
+	case d[0] == 10 && d[1] == 30:
+		p = "ZTEX USB-XMEGA Module 1.0"
+	case d[0] == 10 && d[1] == 40:
+		p = "ZTEX USB-FPGA Module 2.02"
+	case d[0] == 10 && d[1] == 41:
+		p = "ZTEX USB-FPGA Module 2.14"
+	case d[0] == 10 && d[1] == 42:
+		p = "ZTEX USB3-FPGA Module 2.18"
+	case d[0] == 10:
+		p = "ZTEX"
+	}
+	return fmt.Sprintf("%v.%v.%v.%v (%v)", d[0], d[1], d[2], d[3], p)
+}
+
+// Bytes returns a raw representation of the ZTEX product ID.
+func (d DescriptorProduct) Bytes() []byte { return []byte{d[0], d[1], d[2], d[3]} }
+
+// DescriptorFirmware indicates the version of the ZTEX firmware.
+type DescriptorFirmware uint8
+
+// DescriptorInterface indicates the version of the ZTEX interface.
+type DescriptorInterface uint8
+
+// DescriptorCapability indicates the capabilities supported by the ZTEX device.
+type DescriptorCapability [6]uint8
+
+// String returns a human-readable description of the ZTEX capabilities
+// supported by the device.
+func (d DescriptorCapability) String() string {
+	x := []string{}
+	x = append(x, fmt.Sprintf("EEPROM: %v", d.EEPROM()))
+	x = append(x, fmt.Sprintf("FPGA Configuration: %v", d.FPGAConfiguration()))
+	x = append(x, fmt.Sprintf("Flash Memory: %v", d.FlashMemory()))
+	x = append(x, fmt.Sprintf("Debug Helper: %v", d.DebugHelper()))
+	x = append(x, fmt.Sprintf("XMEGA: %v", d.XMEGA()))
+	x = append(x, fmt.Sprintf("High Speed FPGA Configuration: %v", d.HighSpeedFPGAConfiguration()))
+	x = append(x, fmt.Sprintf("MAC EEPROM: %v", d.MACEEPROM()))
+	x = append(x, fmt.Sprintf("MultiFPGA: %v", d.MultiFPGA()))
+	x = append(x, fmt.Sprintf("Temperature Sensor: %v", d.TemperatureSensor()))
+	x = append(x, fmt.Sprintf("Flash Memory 2: %v", d.FlashMemory2()))
+	x = append(x, fmt.Sprintf("FX3 Firmware: %v", d.FX3Firmware()))
+	x = append(x, fmt.Sprintf("Debug Helper 2: %v", d.DebugHelper2()))
+	x = append(x, fmt.Sprintf("Default Firmware Interface: %v", d.DefaultFirmwareInterface()))
+	return strings.Join(x, ", ")
+}
+
+// Function cap returns true if and only if ZTEX capability i.j is
+// supported by the device.
+func (d DescriptorCapability) cap(i, j uint) bool { return d[i]&(1<<j) != 0 }
+
+// EEPROM returns true if and only if the device has EEPROM support.
+func (d DescriptorCapability) EEPROM() bool { return d.cap(0, 0) }
+
+// FPGAConfiguration returns true if and only if the device has basic
+// FPGA configuration support.
+func (d DescriptorCapability) FPGAConfiguration() bool { return d.cap(0, 1) }
+
+// FlashMemory returns true if and only if the device has flash memory.
+func (d DescriptorCapability) FlashMemory() bool { return d.cap(0, 2) }
+
+// DebugHelper returns true if and only if the device has basic debug
+// helper support.
+func (d DescriptorCapability) DebugHelper() bool { return d.cap(0, 3) }
+
+// XMEGA returns true if and only if the device has XMEGA support.
+func (d DescriptorCapability) XMEGA() bool { return d.cap(0, 4) }
+
+// HighSpeedFPGAConfiguration returns true if and only if the device
+// supports high-speed FPGA configuration.
+func (d DescriptorCapability) HighSpeedFPGAConfiguration() bool { return d.cap(0, 5) }
+
+// MACEEPROM returns true if and only if the device has MAC EEPROM support.
+func (d DescriptorCapability) MACEEPROM() bool { return d.cap(0, 6) }
+
+// MultiFPGA returns true if and only if the device has multi-FPGA support.
+func (d DescriptorCapability) MultiFPGA() bool { return d.cap(0, 7) }
+
+// TemperatureSensor returns true if and only if the device has
+// temperature sensor support.
+func (d DescriptorCapability) TemperatureSensor() bool { return d.cap(1, 0) }
+
+// FlashMemory2 returns true if and only if the device has advanced
+// flash memory support.
+func (d DescriptorCapability) FlashMemory2() bool { return d.cap(1, 1) }
+
+// FX3Firmware returns true if and only if the device has FX3 firmware
+// support.
+func (d DescriptorCapability) FX3Firmware() bool { return d.cap(1, 2) }
+
+// DebugHelper2 returns true if and only if the device has advanced debug
+// helper support.
+func (d DescriptorCapability) DebugHelper2() bool { return d.cap(1, 3) }
+
+// DefaultFirmwareInterface returns true if and only if the device
+// supports the default firmware interface.
+func (d DescriptorCapability) DefaultFirmwareInterface() bool { return d.cap(1, 4) }
+
+// DescriptorModule represents product specific configuration.
+type DescriptorModule [12]uint8
+
+// DescriptorSerial represents the device serial number.
+type DescriptorSerial [10]uint8
+
+// DescriptorConfig represents the ZTEX device descriptor.
+type DescriptorConfig struct {
+	DescriptorSize
+	DescriptorVersion
+	DescriptorMagic
+	DescriptorProduct
+	DescriptorFirmware
+	DescriptorInterface
+	DescriptorCapability
+	DescriptorModule
+	DescriptorSerial
+}
--- device.go
+++ device.go
@@ -30,7 +30,7 @@ func (d DeviceConfig) String() string {
 type Device struct {
 	*gousb.Device
 
-	ZTEXConfig
+	DescriptorConfig
 	DeviceConfig
 }
 
@@ -45,50 +45,47 @@ func ControlTimeout(timeout time.Duration) DeviceOption {
 	}
 }
 
-// OpenDevice opens a ZTEX USB-FPGA module and returns its device handle.
-// If there are multiple modules present, then one is chosen arbitrarily.
-func OpenDevice(ctx *gousb.Context, opt ...DeviceOption) (*Device, error) {
-	d := &Device{}
-	if dev, err := ctx.OpenDeviceWithVIDPID(VendorID, ProductID); err != nil {
-		return nil, fmt.Errorf("(*gousb.Context).OpenDeviceWithVIDPID: %v", err)
-	} else if dev == nil {
-		return nil, fmt.Errorf("(*gousb.Context).OpenDeviceWithVIDPID: got nil device, want non-nil device")
-	} else {
-		d.Device = dev
-	}
-
-	b := make([]byte, 128)
+func (d *Device) readDescriptorConfig() error {
+	b := make([]byte, 40)
 
 	// VR 0x22: ZTEX descriptor: read ZTEX descriptor
 	if nbr, err := d.Control(0xc0, 0x22, 0, 0, b); err != nil {
-		return nil, fmt.Errorf("(*gousb.Device).Control: ZTEX descriptor: read ZTEX descriptor: %v", err)
+		return fmt.Errorf("(*ztex.Device).Control: ZTEX descriptor: read ZTEX descriptor: %v", err)
 	} else if nbr != 40 {
-		return nil, fmt.Errorf("(*gousb.Device).Control: ZTEX descriptor: read ZTEX descriptor: got %v bytes, want %v bytes", nbr, 40)
+		return fmt.Errorf("(*ztex.Device).Control: ZTEX descriptor: read ZTEX descriptor: got %v bytes, want %v bytes", nbr, 40)
 	} else if b[0] != 40 {
-		return nil, fmt.Errorf("(*gousb.Device).Control: ZTEX descriptor: read ZTEX descriptor: got size %v, want size %v", b[0], 40)
+		return fmt.Errorf("(*ztex.Device).Control: ZTEX descriptor: read ZTEX descriptor: got size %v, want size %v", b[0], 40)
 	} else if b[1] != 1 {
-		return nil, fmt.Errorf("(*gousb.Device).Control: ZTEX descriptor: read ZTEX descriptor: got version %v, want version %v", b[0], 1)
+		return fmt.Errorf("(*ztex.Device).Control: ZTEX descriptor: read ZTEX descriptor: got version %v, want version %v", b[0], 1)
 	}
-	d.ZTEXConfig = ZTEXConfig{
-		ZTEXSize(b[0]),
-		ZTEXVersion(b[1]),
-		ZTEXMagic([4]uint8{b[2], b[3], b[4], b[5]}),
-		ZTEXProduct([4]uint8{b[6], b[7], b[8], b[9]}),
-		ZTEXFirmware(b[10]),
-		ZTEXInterface(b[11]),
-		ZTEXCapability([6]uint8{b[12], b[13], b[14], b[15], b[16], b[17]}),
-		ZTEXModule([12]uint8{b[18], b[19], b[20], b[21], b[22], b[23], b[24], b[25], b[26], b[27], b[28], b[29]}),
-		ZTEXSerial([10]uint8{b[30], b[31], b[32], b[33], b[34], b[35], b[36], b[37], b[38], b[39]}),
+
+	d.DescriptorConfig = DescriptorConfig{
+		DescriptorSize(b[0]),
+		DescriptorVersion(b[1]),
+		DescriptorMagic([4]uint8{b[2], b[3], b[4], b[5]}),
+		DescriptorProduct([4]uint8{b[6], b[7], b[8], b[9]}),
+		DescriptorFirmware(b[10]),
+		DescriptorInterface(b[11]),
+		DescriptorCapability([6]uint8{b[12], b[13], b[14], b[15], b[16], b[17]}),
+		DescriptorModule([12]uint8{b[18], b[19], b[20], b[21], b[22], b[23], b[24], b[25], b[26], b[27], b[28], b[29]}),
+		DescriptorSerial([10]uint8{b[30], b[31], b[32], b[33], b[34], b[35], b[36], b[37], b[38], b[39]}),
 	}
+
+	return nil
+}
+
+func (d *Device) readDeviceConfig() error {
+	b := make([]byte, 128)
 
 	// VR 0x3b: MAC EEPROM support: read from MAC EEPROM
 	if nbr, err := d.Control(0xc0, 0x3b, 0, 0, b); err != nil {
-		return nil, fmt.Errorf("(*gousb.Device).Control: MAC EEPROM support: read from MAC EEPROM: %v", err)
+		return fmt.Errorf("(*ztex.Device).Control: MAC EEPROM support: read from MAC EEPROM: %v", err)
 	} else if nbr != 128 {
-		return nil, fmt.Errorf("(*gousb.Device).Control: MAC EEPROM support: read from MAC EEPROM: got %v bytes, want %v bytes", nbr, 128)
+		return fmt.Errorf("(*ztex.Device).Control: MAC EEPROM support: read from MAC EEPROM: got %v bytes, want %v bytes", nbr, 128)
 	} else if b[0] != 'C' || b[1] != 'D' || b[2] != '0' {
-		return nil, fmt.Errorf("(*gousb.Device).Control: MAC EEPROM support: read from MAC EEPROM: got signature %v, want signature %v", b[:3], []byte{'C', 'D', '0'})
+		return fmt.Errorf("(*ztex.Device).Control: MAC EEPROM support: read from MAC EEPROM: got signature %v, want signature %v", b[:3], []byte{'C', 'D', '0'})
 	}
+
 	d.DeviceConfig = DeviceConfig{
 		BoardConfig{
 			BoardType(b[3]),
@@ -114,6 +111,29 @@ func OpenDevice(ctx *gousb.Context, opt ...DeviceOption) (*Device, error) {
 		},
 	}
 
+	return nil
+}
+
+// OpenDevice opens a ZTEX USB-FPGA module and returns its device handle.
+// If there are multiple modules present, then one is chosen arbitrarily.
+func OpenDevice(ctx *gousb.Context, opt ...DeviceOption) (*Device, error) {
+	d := &Device{}
+	if dev, err := ctx.OpenDeviceWithVIDPID(VendorID, ProductID); err != nil {
+		return nil, fmt.Errorf("(*gousb.Context).OpenDeviceWithVIDPID: %v", err)
+	} else if dev == nil {
+		return nil, fmt.Errorf("(*gousb.Context).OpenDeviceWithVIDPID: got nil device, want non-nil device")
+	} else {
+		d.Device = dev
+	}
+
+	if err := d.readDescriptorConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := d.readDeviceConfig(); err != nil {
+		return nil, err
+	}
+
 	for _, o := range opt {
 		if err := o(d); err != nil {
 			return nil, err
@@ -126,7 +146,7 @@ func OpenDevice(ctx *gousb.Context, opt ...DeviceOption) (*Device, error) {
 // ResetFX3 resets the Cypress CYUSB3033 EZ-USB FX3S controller on the
 // device, if one is present.
 func (d *Device) ResetFX3() error {
-	if !d.ZTEXCapability.FX3Firmware() {
+	if !d.DescriptorConfig.FX3Firmware() {
 		return fmt.Errorf("(*ztex.Device).ResetFX3: operation not supported")
 	}
 
@@ -141,7 +161,7 @@ func (d *Device) ResetFX3() error {
 
 // FPGAStatus retrieves the current status of the FPGA on the device.
 func (d *Device) FPGAStatus() (*FPGAStatus, error) {
-	if !d.ZTEXCapability.FPGAConfiguration() {
+	if !d.DescriptorConfig.FPGAConfiguration() {
 		return nil, fmt.Errorf("(*ztex.Device).FPGAStatus: operation not supported")
 	}
 
@@ -166,7 +186,7 @@ func (d *Device) FPGAStatus() (*FPGAStatus, error) {
 
 // ResetFPGA resets the FPGA on the device, if one is present.
 func (d *Device) ResetFPGA() error {
-	if !d.ZTEXCapability.FPGAConfiguration() {
+	if !d.DescriptorConfig.FPGAConfiguration() {
 		return fmt.Errorf("(*ztex.Device).ResetFPGA: operation not supported")
 	}
 
